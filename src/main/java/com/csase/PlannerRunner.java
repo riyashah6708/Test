890be77bc8d0ba18d0b2de1898d42e08afe9e943
sophie <wang.sophie@outlook.com>
2025-05-08 20:35:22 -0700

    second commit

--- src/main/java/com/csase/PlannerRunner.java
+++ src/main/java/com/csase/PlannerRunner.java
@@ -5,9 +5,10 @@ public class PlannerRunner {
 
         // Create a new student
         Student jada = new Student();
-
+        Student sophie = new Student();
         // Gets the student's choice until the student exits
         jada.getChoice();
+        sophie.getChoice();
 
     }
 }
